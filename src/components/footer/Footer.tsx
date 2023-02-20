0d45a12c753e78878ce1d6bbcf8f38c4a9843b24
Andres Artunduaga <andres.artunduaga@outlook.com>
2023-02-20 00:53:41 -0500

    refactor footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -9,6 +9,8 @@ import { SITE_LINKS, CONTACT_DATA } from "@constants";
 
 export const Footer: React.FC = () => {
 
+    const BASE_CLASS = "footer";
+
     const socialData: Array<{ name: IconName, link: string }> = [
         { name: "facebook", link: "https://facebook.com" },
         { name: "instagram", link: "https://instagram.com" },
@@ -17,34 +19,35 @@ export const Footer: React.FC = () => {
     ]
 
     return (
-        <footer className="footer">
-            <div className="wrapper">
-                <span className="footer-logo">
+        <footer className={BASE_CLASS}>
+            <div className={`${BASE_CLASS}-wrapper`}>
+                <span className={`${BASE_CLASS}-logo`}>
                     <Logo />
                 </span>
-                <div className="columns">
-                    <div className="column contact-info">
-                        <span className="column-title">Datos de contacto</span>
+                <div className={`${BASE_CLASS}-columns`}>
+                    <div className={`${BASE_CLASS}-columns-column contact-info`}>
+                        <span className={`${BASE_CLASS}-columns-column-title`}>Datos de contacto</span>
                         <br />
-                        <span className="contact-info-item">
-                            <Icon name="mail" className="contact-info-item-icon" variant="primary"></Icon>
-                            <a className="footer-link" href={`mailto:${CONTACT_DATA.CONTACT_MAIL}`}>{CONTACT_DATA.CONTACT_MAIL}</a>
-                        </span>
-                        <span className="contact-info-item">
-                            <Icon name="phone" className="contact-info-item-icon" variant="primary"></Icon>
-                            <a className="footer-link" href={`tel:${CONTACT_DATA.CONTACT_PHONE}`}>{CONTACT_DATA.CONTACT_PHONE}</a>
-                        </span>
-                        <span className="contact-info-item">
-                            <Icon name="place" className="contact-info-item-icon" variant="primary"></Icon>
-                            <a className="footer-link" href="https://goo.gl/maps/UZ9S8g1y32B8voN9A">{CONTACT_DATA.CONTACT_ADDRESS}</a>
-                        </span>
+                            <a className={`${BASE_CLASS}-link`} href={`mailto:${CONTACT_DATA.CONTACT_MAIL}`}>
+                                <Icon name="mail" className={`${BASE_CLASS}-link-icon`} variant="primary"></Icon>
+                                {CONTACT_DATA.CONTACT_MAIL}
+                            </a>
+                            <a className={`${BASE_CLASS}-link`} href={`tel:${CONTACT_DATA.CONTACT_PHONE}`}>
+                                <Icon name="phone" className={`${BASE_CLASS}-link-icon`} variant="primary"></Icon>
+                                {CONTACT_DATA.CONTACT_PHONE}
+                            </a>
+
+                            <a className={`${BASE_CLASS}-link`} href="https://goo.gl/maps/UZ9S8g1y32B8voN9A">
+                                <Icon name="place" className={`${BASE_CLASS}-link-icon`} variant="primary"></Icon>
+                                {CONTACT_DATA.CONTACT_ADDRESS}
+                            </a>
                     </div>
-                    <div className="column links">
-                    <span className="column-title">Enlaces</span>
+                    <div className={`${BASE_CLASS}-columns-column links`}>
+                    <span  className={`${BASE_CLASS}-columns-column-title`}>Enlaces</span>
                     <br />
                         {
                             SITE_LINKS.map(({ link, label }, idx) => {
-                                return (<Link key={idx} className="links-item footer-link" activeClassName="footer-link-active" to={link}>{label}</Link>)
+                                return (<Link key={idx} className="footer-link" activeClassName="footer-link-active" to={link}>{label}</Link>)
                             })
                         }
                     </div>
