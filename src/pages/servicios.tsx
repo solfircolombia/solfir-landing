666303e2d663eec8c7f83e91f5d8c820167c1a4a
Andres Artunduaga <andres.artunduaga@outlook.com>
2023-02-24 02:00:55 -0500

    adding services page

---
+++ src/pages/servicios.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { HeadFC, Link, PageProps } from 'gatsby';
+import { Layout } from '@components';
+import { STATIC_SITE_LABELS, STATIC_SITE_LINKS } from '@constants';
+import "./servicios.scss";
+
+
+const ThanksPage: React.FC<PageProps> = () => {
+
+    const BASE_CLASS = 'gracias';
+
+    return (
+        <Layout>
+            <div className={BASE_CLASS}>
+                <div className={`${BASE_CLASS}-wrapper`} >
+                    <div className={`${BASE_CLASS}-message-box`} >
+                        <span className={`${BASE_CLASS}-message-box-title`}>Servicios</span>
+                        <span className={`${BASE_CLASS}-message-box-text`} >Esta pagina aun se encuentra en construcción</span>
+                        <Link className={`${BASE_CLASS}-message-box-back`} to={STATIC_SITE_LINKS.HOME}>{`Volver al ${STATIC_SITE_LABELS.HOME}`}</Link>
+                    </div>
+                </div>
+            </div>
+            </Layout>
+    );
+}
+
+export default ThanksPage;
+
+export const Head: HeadFC = () => <title>SOLFIR - {STATIC_SITE_LABELS.SERVICES}</title>
