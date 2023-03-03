b19482d130ce279b3e6573b2ebce8c7dfde965f0
Andres Artunduaga <andres.artunduaga@outlook.com>
2023-03-03 00:47:41 -0500

    Adding Champagne&Limousines font

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -10,5 +10,37 @@ export const onRenderBody = ({ setHeadComponents }:{setHeadComponents:any}) => {
       crossOrigin="anonymous"
       key="modern20font"
     />,
+    <link
+      rel="preload"
+      href="/fonts/Champagne&Limousines.ttf"
+      as="font"
+      type="font/ttf"
+      crossOrigin="anonymous"
+      key="Champagne&Limousines"
+    />,
+    <link
+      rel="preload"
+      href="/fonts/Champagne&LimousinesBold.ttf"
+      as="font"
+      type="font/ttf"
+      crossOrigin="anonymous"
+      key="Champagne&LimousinesBold"
+    />,
+    <link
+      rel="preload"
+      href="/fonts/Champagne&LimousinesBoldItalic.ttf"
+      as="font"
+      type="font/ttf"
+      crossOrigin="anonymous"
+      key="Champagne&LimousinesBoldItalic"
+    />,
+    <link
+      rel="preload"
+      href="/fonts/Champagne&LimousinesItalic.ttf"
+      as="font"
+      type="font/ttf"
+      crossOrigin="anonymous"
+      key="Champagne&LimousinesItalic"
+    />,
   ])
 }
