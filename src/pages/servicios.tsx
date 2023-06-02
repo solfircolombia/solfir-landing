234184fafc36c50523b60c0c4dec2c8a630dd54d
Andres Artunduaga <andres.artunduaga@outlook.com>
2023-06-01 23:04:52 -0500

    initial services page

--- src/pages/servicios.tsx
+++ src/pages/servicios.tsx
@@ -1,12 +1,55 @@
-import React from 'react';
-import { HeadFC, Link, PageProps } from 'gatsby';
-import { Layout, SEO } from '@components';
-import { STATIC_SITE_LABELS, STATIC_SITE_LINKS } from '@constants';
+import React, { useState } from 'react';
+import { HeadFC, Link, PageProps, graphql, navigate } from 'gatsby';
+import { Button, Card, Icon, Layout, SEO, Modal, Logo } from '@components';
+import { SITE_LINKS, STATIC_SITE_LABELS, STATIC_SITE_LINKS } from '@constants';
 import { Utils } from '@shared';
 import './servicios.scss';
+import { IconName } from '@types';
 
-const ThanksPage: React.FC<PageProps> = () => {
+const ServicesPage = ({ data }: PageProps<Queries.ServicesPageQuery>) => {
     const BASE_CLASS = 'servicios';
+
+    const SERVICES: { serviceIcon: IconName; serviceName: string; serviceText: string }[] =
+        data.allContentfulService.edges.map(({ node }) => ({
+            serviceIcon: (node.icon as IconName) || '',
+            serviceName: node.title || '',
+            serviceText: node.description?.description || '',
+        }));
+
+    let [openedModal, setOpenedModal] = useState(false);
+    let [modalContent, setModalContent] = useState<React.ReactNode>(null);
+
+    const onClickReadMore = (s: {
+        serviceIcon: IconName;
+        serviceName: string;
+        serviceText: string;
+    }) => {
+        setModalContent(
+            <div className="service-modal-content">
+                <span className="service-modal-content-title">{s.serviceName}</span>
+                <p className="service-modal-content-text">{s.serviceText}</p>
+                <p className="service-modal-content-contact">
+                    ¿Deseas saber mas sobre este servicio?
+                </p>
+                <Button
+                    className="service-modal-content-contact-btn"
+                    variant="primary"
+                    size="small"
+                    onClick={() => {
+                        navigate(STATIC_SITE_LINKS.CONTACT);
+                    }}
+                >
+                    Contáctanos
+                </Button>
+                <span className="service-modal-content-gap"></span>
+            </div>
+        );
+        setOpenedModal(true);
+    };
+
+    const onCloseModal = () => {
+        setOpenedModal(!openedModal);
+    };
 
     return (
         <Layout>
@@ -19,22 +62,58 @@ const ThanksPage: React.FC<PageProps> = () => {
                     </div>
                 </div>
                 <div className={`${BASE_CLASS}-wrapper`}>
-                    <div className={`${BASE_CLASS}-message-box`}>
-                        <span className={`${BASE_CLASS}-message-box-title`}>Servicios</span>
-                        <span className={`${BASE_CLASS}-message-box-text`}>
-                            Esta pagina aun se encuentra en construcción
-                        </span>
-                        <Link
-                            className={`${BASE_CLASS}-message-box-back`}
-                            to={STATIC_SITE_LINKS.HOME}
-                        >{`Volver al ${STATIC_SITE_LABELS.HOME}`}</Link>
+                    <div className={`card-wrapper`}>
+                        {SERVICES.map((s) => {
+                            return (
+                                <Card key={s.serviceName} customClasses="service-card">
+                                    <span className="service-card-icon">
+                                        <Icon name={s.serviceIcon} size={48}></Icon>
+                                    </span>
+                                    <span className="service-card-title">{s.serviceName}</span>
+                                    <Button
+                                        className="service-card-button"
+                                        size="small"
+                                        onClick={() => {
+                                            onClickReadMore(s);
+                                        }}
+                                    >
+                                        Leer Mas
+                                    </Button>
+                                </Card>
+                            );
+                        })}
                     </div>
                 </div>
+                {openedModal && (
+                    <Modal
+                        ariaLabel="Ventana modal menu"
+                        onCloseModal={onCloseModal}
+                        customClass="service-modal"
+                    >
+                        {modalContent}
+                    </Modal>
+                )}
             </div>
         </Layout>
     );
 };
 
-export default ThanksPage;
+export const query = graphql`
+    query ServicesPage {
+        allContentfulService {
+            edges {
+                node {
+                    icon
+                    title
+                    description {
+                        description
+                    }
+                }
+            }
+        }
+    }
+`;
+
+export default ServicesPage;
 
 export const Head: HeadFC = () => <SEO {...Utils.getSEOProps(STATIC_SITE_LINKS.SERVICES)} />;
