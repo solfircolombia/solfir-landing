41668ea25b8d61fd3826a3aff36824a7bc8298b3
Andres Artunduaga <andres.artunduaga@outlook.com>
2024-05-27 18:50:03 -0500

    wip

--- src/components/serviceCard/ServiceCard.tsx
+++ src/components/serviceCard/ServiceCard.tsx
@@ -1,16 +1,32 @@
 import * as React from 'react';
 
 import './serviceCard.scss';
-import { GatsbyImage, getImage } from 'gatsby-plugin-image';
+import { GatsbyImage, IGatsbyImageData } from 'gatsby-plugin-image';
 
 type ServiceCardProps = {
     title: string;
     text: string;
-    image: any;
+    image?: IGatsbyImageData;
 };
 
-export const ServiceCard: React.FC<ServiceCardProps> = () => {
+export const ServiceCard: React.FC<ServiceCardProps> = ({ title, text, image }) => {
     const BASE_CLASS = 'service-card';
 
-    return <></>;
+    return (
+        <div className={BASE_CLASS}>
+            {image && (
+                <div className={`${BASE_CLASS}-image`}>
+                    <GatsbyImage
+                        image={image}
+                        alt={title}
+                        className={`${BASE_CLASS}-image-gatsby`}
+                    />
+                </div>
+            )}
+            <div className={`${BASE_CLASS}-content`}>
+                <h3 className={`title`}>{title}</h3>
+                <p className={`text`}>{text}</p>
+            </div>
+        </div>
+    );
 };
